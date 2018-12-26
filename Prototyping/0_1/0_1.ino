7984de4d191407599c0a35fc9ecdecd99fb8b6af
Felix Cherkasskiy <45081342+fCherkasskiy@users.noreply.github.com>
2018-12-26 15:33:38 -0500

    Update 0_1.ino

--- Prototyping/0_1/0_1.ino
+++ Prototyping/0_1/0_1.ino
@@ -3,29 +3,21 @@
 #define NUM_LEDS 300
 
 CRGB leds[NUM_LEDS];
-bool power[NUM_LEDS];
 
 byte del = 20;
 
 void setup() {
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  for(int i = 0; i <= NUM_LEDS; i ++){
-    power[i] = 1;
-  }
-  for(int i = 50; i <= 100; i ++){
-    power[i] = 0;
-  }
 }
 
 void loop() {
-  tracer(1);
+  tracer(0);
 }
 
 void tracer(bool bounce){ // One LED on at a time, going down the whole strip
   if(bounce == 0){ // one time and then restarts
     for(int i = 0; i < NUM_LEDS; i++){
-      i = checkOn(power[i], i);
-      leds[i] = CRGB(255, 255, 255);
+      leds[i] = CRGB(0, 0, 0);
       FastLED.show();
       delay(del);
       leds[i] = CRGB(0, 0, 0);
@@ -48,12 +40,4 @@ void tracer(bool bounce){ // One LED on at a time, going down the whole strip
       FastLED.show();
     }
   }
-}
-
-int checkOn(bool state, int led){
-  if(state == 0){
-    leds[led] = CRGB(0, 0, 0);
-  }
-  led ++;
-  return led;
 }

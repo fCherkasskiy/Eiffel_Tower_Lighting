52619229854f74cce4de0fc88d38376f53104f13
Felix Cherkasskiy <45081342+fCherkasskiy@users.noreply.github.com>
2018-12-26 15:41:51 -0500

    Update 0_1.ino

--- Prototyping/0_1/0_1.ino
+++ Prototyping/0_1/0_1.ino
@@ -8,6 +8,7 @@ byte del = 20;
 
 void setup() {
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -17,7 +18,11 @@ void loop() {
 void tracer(bool bounce){ // One LED on at a time, going down the whole strip
   if(bounce == 0){ // one time and then restarts
     for(int i = 0; i < NUM_LEDS; i++){
-      leds[i] = CRGB(0, 0, 0);
+      byte r = byte(random(256));
+      byte g = byte(random(256));
+      byte b = byte(random(256));
+      Serial.print(r);      Serial.print("   ");      Serial.print(g);      Serial.print("   ");      Serial.print(b);    Serial.println("");
+      leds[i] = CRGB(r, g, b);
       FastLED.show();
       delay(del);
       leds[i] = CRGB(0, 0, 0);
